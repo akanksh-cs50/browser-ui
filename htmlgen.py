97e4560b5c53e97afbe7331c1087e0a14099d7ff
Akanksh Chitimalla <144524423+akanksh-cs50@users.noreply.github.com>
2024-07-21 02:53:20 +0530

    added a small test python file to test out nerd fonts in html

---
+++ htmlgen.py
@@ -0,0 +1,17 @@
+import os
+
+with open('build/result.html', 'w+') as result:
+
+    result.write("""
+    <style>
+        @import "https://www.nerdfonts.com/assets/css/webfont.css";
+    </style>
+    """)
+
+    for item in os.listdir():
+        if os.path.isfile(item):
+            result.write(f'<i class="nf nf-oct-file_directory"></i> {item}<br>\n')
+        else:
+            result.write(f'<i class="nf nf-cod-file"></i> {item}<br>\n')
+
+    result.close()
