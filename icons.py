62882be1fbe5ba93ea585b6b3547de0294958c8b
Akanksh Chitimalla <144524423+akanksh-cs50@users.noreply.github.com>
2024-07-21 23:38:12 +0530

    added function to return icon name

--- icons.py
+++ icons.py
@@ -8,3 +8,13 @@ Example:
 
 This file will grow as development goes.
 """
+
+associations = {
+    'txt': 'nf nf-fa-file-text',
+    'jpg': 'nf nf-fa-file-image',
+    'png': 'nf nf-fa-file-image',
+    'rs': 'nf nf-dev-rust'
+}
+
+def icon_name(filename:str):
+    return associations[filename.split('.')[-1]] # Using -1 index which gives us the last element in the list.
