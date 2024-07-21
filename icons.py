8acb9518457e07ecfb64c58c49a421d78bea4204
Akanksh Chitimalla <144524423+akanksh-cs50@users.noreply.github.com>
2024-07-21 23:24:42 +0530

    added icons.py for giving icon name given a file extension

---
+++ icons.py
@@ -0,0 +1,10 @@
+"""
+This file is used for giving the icon name when given a file extension.
+
+Example:
+
+.rs -> nf nf-dev-rust
+.png -> nf-md-file_png_box
+
+This file will grow as development goes.
+"""
