a07db2a898808f6fbf259a16d8e4adcea812ea8d
Akanksh Chitimalla <144524423+akanksh-cs50@users.noreply.github.com>
2024-07-23 21:33:07 +0100

    got basic search working

--- static/search.js
+++ static/search.js
@@ -1,3 +1,19 @@
 document.querySelector('input').addEventListener('keypress', function(){
-	console.log('key was pressed in the <input> tag')
+	item_elements = document.querySelectorAll('a')
+	items = []
+
+	for(i = 0; i < item_elements.length; i++) {
+		items.push(item_elements[i].textContent.replace(' ', ''))
+	}
+
+	for(i = 0; i < item_elements.length; i++) {
+		if (items[i].includes(
+			document.querySelector('input').value)
+		){
+			item_elements[i].style.display = "block"
+		}else{
+			item_elements[i].style.display = "none"
+		}
+	}
+
 })
