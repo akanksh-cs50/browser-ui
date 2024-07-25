1589c70c8e884500173a7cd9ce500922ea38b2da
Akanksh Chitimalla <144524423+akanksh-cs50@users.noreply.github.com>
2024-07-25 10:57:58 +0100

    removing trailing slashes from BASE_DIR

--- app.py
+++ app.py
@@ -10,7 +10,10 @@ app.jinja_env.filters["icon_name"] = icon_name
 @app.route('/favicon.ico')
 def favicon():
     return redirect('/')
-BASE_DIR = '/home/akanksh/Music'
+BASE_DIR = '/home/akanksh'
+
+# trailing slash needs to be removed now because of how the base directory is implemented in templates/ui.html
+BASE_DIR = BASE_DIR[1:] if BASE_DIR.endswith('/') else BASE_DIR
 
 
 @app.route('/', defaults={'current_dir': ''})
